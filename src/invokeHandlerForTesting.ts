5b26a44d800722c9cf7cc258cb30c57a5e0287f0
Ulad Kasach <uladkasach@gmail.com>
2021-12-04 06:14:46 -0700

    feat(types): ensure that handler return and event type are properly inferred from handler fn

--- src/invokeHandlerForTesting.ts
+++ src/invokeHandlerForTesting.ts
@@ -17,15 +17,15 @@ export const invokeHandlerForTesting = async <E, R = any>({
 }: {
   event: E;
   context?: Record<string, any>;
-  handler: Handler;
+  handler: Handler<E, R>;
 }): Promise<R> =>
   new Promise((resolve, reject) => {
     const response = handler(
       stripInvocationEvent(event),
       context as Context, // cast "as Context", since Context could have more keys than the ones defined here. also, if there is less, for usage to invoke handler functions there is no con
-      (error: any, result: R) => {
+      (error: any, result: R | undefined) => {
         if (error) return reject(error);
-        return resolve(result); // resolve result from callback
+        return resolve(result!); // resolve result from callback
       },
     );
     if (response instanceof Promise) resolve(response); // if it was an async function and returned something, resolve that; otherwise, the callback will get triggered
